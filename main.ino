d72859f26cbf7e6f0f9ad124f7d09947f5d1608f
Abhijeet Kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-10-04 22:12:01 +0530

    Refactored !
    removed unwanted lines
    reduced code size
    improved code readability & formatting

--- main.ino
+++ main.ino
@@ -1,4 +1,3 @@
-
 // Libraries
 
 #include <SPI.h>
@@ -8,127 +7,110 @@
 
 // Display setup
 
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_WIDTH 128  // OLED display width, in pixels
+#define SCREEN_HEIGHT 64  // OLED display height, in pixels
 
-#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
+#define OLED_RESET -1        // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C  ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
 
 
 // GPIO
 
-#define RED_led 2 // pin D2
-#define LDR A0 // pin A0
-#define Ir A1 // pin A1
+#define RED_ledPin 2      // pin D2
+#define LDR_sensorPin A0  // pin A0
+#define IR_sensorPin A1   // pin A1
 
+// variables
 
-//Variables
+float NDVI = 0;             //  the ndvi value
+float IR_sensorValue = 0;   // ir sensor value
+float RED_sensorValue = 0;  // ldr sensor value
 
-float NDVI = 0;
-float ADC_ir = 0;
-float ADC_red = 0;
-float IR =  0;
-float RED = 0;
-
-const int IR_Max = 1023; // Put Max Value of IR reflectance (Calibration Value)
-const int RED_Max = 1023 ; // Put Max Value of RED reflectance (Calibration Value)
+const int IR_sensorMax = 0;   // maximum reflectance value of IR sensor  (calibration value)
+const int RED_sensorMax = 0;  // maximum reflectance value of LDR sensor (calibration value)
 
 
 void setup() {
 
   // Debug console
-  Serial.begin(9600); // at 9600 baud rate
-
+  Serial.begin(9600);  // at 9600 baud rate
 
   //Dislay begin
-
-  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS); // Display type, I2C address
-  display.clearDisplay();  // clear
-
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);  // Display type, I2C address
+  display.clearDisplay();                               // clear
 
   // I/O declaration
+  pinMode(RED_ledPin, OUTPUT);
+  pinMode(LDR_sensorPin, INPUT);
+  pinMode(IR_sensorPin, INPUT);
 
-  pinMode(RED_led, OUTPUT); // delared as output
-  pinMode(LDR, INPUT); // delared as input
-  pinMode(Ir, INPUT);
-
-
-  Serial.println("NDVI-GO begin successfully !"); // print initialization message
+  Serial.println("NDVI-GO begin successfully !");  // print initialization message
+  Serial.println("");
+  Serial.println("Getting data........");
   Serial.println("");
 
   // print message to display
-
   display.setTextSize(2);
   display.setTextColor(WHITE);
-  display.setCursor(15, 15); // (x,y)
+  display.setCursor(15, 15);  // (x,y)
   display.println("NDVI-GO");
   display.display();
 
   delay(1000);
-
-
 }
 
 void loop() {
 
 
-  //  ***************** Measure analog value using cahnnel A0 & A1 ******************
+  // read the sensor:
+
+  IR_sensorValue = abs(analogRead(IR_sensorPin) - 1023);  // get reading for IR
+  delay(200);
+
+  digitalWrite(RED_ledPin, HIGH);  // turn on RED lED
+  delay(200);
+
+  RED_sensorValue = abs(analogRead(LDR_sensorPin) - 1023);  // get reading for RED/LDR
+  delay(200);
+
+  digitalWrite(RED_ledPin, LOW);
 
 
-  Serial.print("Getting data........"); // print a message
-  delay(500);
-
-
-  // get reading for IR:
-  ADC_ir = abs(analogRead(A0) - 1023); //reads the value of the IR sensor (value between 0 and 1023)
-  delay(1000);
-
-
-  // get reading for RED:
-  digitalWrite(RED_led, HIGH); // turn on RED lED
-
-  delay(200);//wait
-  ADC_red = abs(analogRead(A1) - 1023); // reads the value of the LDR
-  delay(1000);
-
-  digitalWrite(RED_led, LOW); // turn off RED lED
-
-
-
-  //  *************** Calculate NDVI ******************
-
-
-  IR = map(ADC_ir, 0, IR_Max, 0, 100); // scale it for use (value between 0 and 100)
-  IR /= 100;
-
-  RED = map(ADC_red, 0, RED_Max, 0, 100);
-  RED /= 100;
-
-  NDVI = float((float(IR) - float(RED)) / (float(IR) + float(RED)));  // NDVI expression
+  // print on console
+  Serial.println(" ");
+  Serial.println("IR sensor Value :");
+  Serial.print(IR_sensorValue);
+  Serial.println("RED sensor Value :");
+  Serial.print(RED_sensorValue);
 
 
 
-  //  ***************** Print analog values & NDVI to console ******************
+  // apply the calibration to the sensor reading:
 
+  IR_sensorValue = map(IR_sensorValue, 0, IR_sensorMax, 0, 100);  // (0 - 100)
+
+  // in case the sensor value is outside the range seen during calibration
+  IR_sensorValue = constrain(IR_sensorValue, 0, 100);
+
+  RED_sensorValue = map(RED_sensorValue, 0, RED_sensorMax, 0, 100);  // (0 - 100)
+
+  // in case the sensor value is outside the range seen during calibration
+  RED_sensorValue = constrain(RED_sensorValue, 0, 100);
+
+
+
+  // calculate NDVI using the calibrated value:
+
+  NDVI = float((float(IR_sensorValue / 100) - float(RED_sensorValue)) / (float(IR_sensorValue) + float(RED_sensorValue)));  // NDVI expression
+
+  // print on console
   Serial.println("");
-  Serial.println("");
-  Serial.print("ADC ir Value: "); // debug data
-  Serial.print(ADC_ir);
-  Serial.print(", ADC red Value: ");
-  Serial.print(ADC_red);
+  Serial.println("NDVI Value :");
+  Serial.print(NDVI);
 
-  Serial.print(", IR Value : ");
-  Serial.print(IR);
-  Serial.print(", RED Value: ");
-  Serial.print(RED);
-  Serial.print(", NDVI Value: ");
-  Serial.println(NDVI);
-  Serial.println("");
 
   // print to display
-
   display.clearDisplay();  // clear
   display.setTextSize(2);
   display.setTextColor(WHITE);
@@ -138,30 +120,30 @@ void loop() {
   display.println(NDVI);
   display.display();
 
+  /*
+  // Remarks :
 
-  // Remarks
-
-  if (NDVI >= -1 && NDVI <= 0 ) { // (-1 to 0)
+  if (NDVI >= -1 && NDVI <= 0) {  // (-1 to 0)
     Serial.println("Dead plant or Inanimate object !");
     Serial.println(" ");
   }
 
-  else if (NDVI >= 0 && NDVI <= 0.33 ) { // (0 to 0.33)
+  else if (NDVI >= 0 && NDVI <= 0.33) {  // (0 to 0.33)
     Serial.println("Unhealthy plant !");
     Serial.println(" ");
   }
 
-  else if (NDVI >= 0.33 && NDVI <= 0.66) { // (0.33 to 0.66)
+  else if (NDVI >= 0.33 && NDVI <= 0.66) {  // (0.33 to 0.66)
     Serial.println("Moderatly healthy plant !");
     Serial.println(" ");
   }
 
-  else if (NDVI >= 0.66 && NDVI <= 0.33) { // (0.66 to 1)
+  else if (NDVI >= 0.66 && NDVI <= 0.33) {  // (0.66 to 1)
     Serial.println("Very healthy plant !");
     Serial.println(" ");
   }
+*/
 
-
-  delay(1000); // delay
-
+  // delay a little bit
+  delay(1000);  // delay
 }
